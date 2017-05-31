256b321d0a5c92708c8deb79b3d54c3f72a13bfb
Gabriel Henrique Rudey <ucraaa@gmail.com>
2017-05-31 02:35:27 -0300

    Create `snake` module and basic movement mechanics
    - Move snake logic to module Snake
    - Listen to arrow keys to move Snake head

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
 extern crate sdl2;
 
+mod snake;
+
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::{ Rect, Point };
 use std::thread;
 use std::time::Duration;
+use snake::*;
 
-struct Snake {
-    velocity: u32,
-    tail_len: u8,
-    parts: Vec<SnakeParts>
-}
-
-// Snake (x, y)
-enum SnakeParts {
-    Head(u32, u32),
-    Tail(u32, u32)
-}
-
-enum Facing {
-    Top,
-    Bottom,
-    Left,
-    Right
-}
+// Tiles are 32x32
+const TILE: u8 = 32;
 
 fn main() {
+    let BLACK: Color = Color::RGB(0, 0, 0);
+    let WHITE: Color = Color::RGB(0xff, 0xff, 0xff);
+    let RED: Color   = Color::RGB(0xff, 0x00, 0x00);
+    let GREEN: Color = Color::RGB(0x00, 0xff, 0x00);
+
     let ctx   = sdl2::init().expect("Could not create a SDL2 context");
     let video = ctx.video().expect("Could not create a video subsystem");
 
@@ -40,20 +32,31 @@ fn main() {
         .build()
         .expect("Could not create a canvas!");
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.set_draw_color(BLACK);
     canvas.clear();
     canvas.present();
 
     let mut timer = ctx.timer().expect("Could not create a timer!");
     let mut events = ctx.event_pump().expect("Event pump was not created!");
+    // TODO: Calc framerate based on thread::sleep
     let mut framerate = 0u16;
-
-    canvas.set_draw_color(Color::RGB(255, 255, 255));
-    let mut rect = Rect::new(0, 0, 32, 32);
+    let mut snake = Snake::new(1, GREEN);
 
     'running: loop {
         for event in events.poll_iter() {
             match event {
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
+                    snake.look_to(Facing::Top);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
+                    snake.look_to(Facing::Bottom);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
+                    snake.look_to(Facing::Left);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
+                    snake.look_to(Facing::Right);
+                },
                 Event::KeyDown { keycode: Some(_), .. } => {
                     break 'running;
                 },
@@ -61,8 +64,16 @@ fn main() {
             }
         }
 
-        canvas.fill_rect(rect);
+        let ticks = timer.ticks();
+
+        canvas.set_draw_color(BLACK);
+        canvas.clear();
+
+        snake.draw(&mut canvas);
+
         canvas.present();
+
+        snake.move_forward();
 
         thread::sleep(Duration::from_millis(100));
     }
---
+++ src/snake.rs
@@ -0,0 +1,81 @@
+use sdl2;
+use super::TILE;
+
+pub struct Snake {
+    velocity: u8, // Tiles per move_forward()
+    color: sdl2::pixels::Color,
+    direction: Facing,
+    tail_len: u8,
+    head: sdl2::rect::Rect,
+    tail: Vec<sdl2::rect::Rect>
+}
+
+pub enum Facing {
+    Top,
+    Bottom,
+    Left,
+    Right
+}
+
+impl Snake {
+    pub fn new(v: u8, c: sdl2::pixels::Color) -> Self {
+        Self {
+            velocity: v,
+            color: c,
+            tail_len: 5,
+            direction: Facing::Right,
+            head: sdl2::rect::Rect::new(0, 0, TILE as u32, TILE as u32),
+            tail: vec![
+                sdl2::rect::Rect::new(0, 0, TILE as u32, TILE as u32),
+                sdl2::rect::Rect::new(0, 0, TILE as u32, TILE as u32),
+                sdl2::rect::Rect::new(0, 0, TILE as u32, TILE as u32),
+                sdl2::rect::Rect::new(0, 0, TILE as u32, TILE as u32),
+                sdl2::rect::Rect::new(0, 0, TILE as u32, TILE as u32)
+            ]
+        }
+    }
+
+    pub fn draw(&self, canvas: &mut sdl2::render::WindowCanvas) {
+        canvas.set_draw_color(self.color);
+        canvas.fill_rect(self.head);
+
+        for r in self.tail.clone() {
+            canvas.fill_rect(r);
+        }
+    }
+
+    // TODO: Name method 'better'
+    pub fn look_to(&mut self, direction: Facing) {
+        self.direction = direction;
+    }
+
+    // TODO: Snake couldn't go reverse! E.g. left to right or bottom-up
+    pub fn move_forward(&mut self) {
+        let (x, y) = (self.head.x(), self.head.y());
+
+        match self.direction {
+            Facing::Top => {
+                self.head.set_x(x);
+                self.head.set_y(y - (TILE * self.velocity) as i32);
+            },
+            Facing::Bottom => {
+                self.head.set_x(x);
+                self.head.set_y(y + (TILE * self.velocity) as i32);
+            },
+            Facing::Left => {
+                self.head.set_x(x - (TILE * self.velocity) as i32);
+                self.head.set_y(y);
+            },
+            Facing::Right => {
+                self.head.set_x(x + (TILE * self.velocity) as i32);
+                self.head.set_y(y);
+            }
+        }
+    }
+
+    // Move the tail according to "parent" tile
+    fn move_tail(&mut self) {
+        unimplemented!()
+    }
+}
+
